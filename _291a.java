1e24e87cf16ef61b8d85972861398260576a0eb6
bound1ess <bound1ess.dev@gmail.com>
2015-05-15 21:34:17 +0600

    Add 291a.

---
+++ _291a.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class _291a {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int[] id = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            id[i] = scan.nextInt();
+        }
+
+        Arrays.sort(id);
+
+        int ans = 0, val = id[0], streak = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (id[i] == val) {
+                streak++;
+            } else {
+                if (val > 0 && streak > 2) {
+                    System.out.println(-1);
+                    return;
+                }
+
+                if (val > 0 && streak == 2) {
+                    ans++;
+                }
+
+                val = id[i];
+                streak = 1;
+            }
+        }
+
+        if (val > 0 && streak > 2) {
+            System.out.println(-1);
+        } else {
+            if (val > 0 && streak == 2) {
+                //System.out.println(val);
+                ans++;
+            }
+
+            System.out.println(ans);
+        }
+    }
+}
