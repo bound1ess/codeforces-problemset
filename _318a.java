1ed587cfd06833dee72890cd501bdb37ec4fea18
bound1ess <bound1ess.dev@gmail.com>
2015-05-15 16:30:15 +0600

    Add 318a.

---
+++ _318a.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class _318a {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        BigInteger n = in.nextBigInteger(), k = in.nextBigInteger();
+
+        in.close();
+
+        BigInteger j = n.add(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2));
+
+        if (j.compareTo(k) >= 0) {
+            System.out.println(
+                k.multiply(BigInteger.valueOf(2)).subtract(BigInteger.valueOf(1))
+            );
+        } else {
+            System.out.println(
+                k.subtract(
+                    n.add(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2))
+                ).multiply(BigInteger.valueOf(2))
+            );
+        }
+    }
+}
