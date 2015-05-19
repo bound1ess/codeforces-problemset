b23f6f229d528c3f03b89e4d5bbcda8aba68cbf3
bound1ess <bound1ess.dev@gmail.com>
2015-05-19 18:40:23 +0600

    Add 253a.

---
+++ _253a.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.io.File;
+//import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
+
+public class _253a {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(new File("input.txt"));
+        //System.setOut(new PrintStream(new FileOutputStream(new File("output.txt"))));
+        PrintStream out = new PrintStream("output.txt");
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = (n > m) ? m : n;
+
+        for (int i = 0; i < (n - k); i++) {
+            out.print('B');
+        }
+
+        for (int i = 0; i < k; i++) {
+            out.print("GB");
+        }
+
+        for (int i = 0; i < (m - k); i++) {
+            out.print('G');
+        }
+
+        out.print("\n");
+        out.close();
+    }
+}
