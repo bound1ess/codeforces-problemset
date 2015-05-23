fd8828e24e6f96e8cdc34386b5b0350e28293955
bound1ess <bound1ess.dev@gmail.com>
2015-05-24 00:03:55 +0600

    Add 291b.

---
+++ 291/b.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class b {
+
+    public static void main(String[] _args) {
+        Scanner in = new Scanner(System.in);
+        String cmd = in.nextLine();
+        in.close();
+
+        ArrayList<String> args = new ArrayList<>();
+
+        boolean asIs = false;
+        StringBuilder buf = new StringBuilder();
+
+        for (int i = 0; i < cmd.length(); i++) {
+            if (cmd.charAt(i) == '"') {
+                asIs = ! asIs;
+
+                if ( ! asIs) {
+                    args.add(buf.toString());
+                    buf.setLength(0);
+                }
+
+                continue;
+            }
+
+            if ( ! asIs && cmd.charAt(i) == ' ' && buf.length() > 0) {
+                args.add(buf.toString());
+                buf.setLength(0);
+                continue;
+            }
+
+            if (asIs || cmd.charAt(i) != ' ') {
+                buf.append(cmd.charAt(i));
+            }
+        }
+
+        if (buf.length() > 0) {
+            args.add(buf.toString());
+        }
+
+        for (int i = 0; i < args.size(); i++) {
+            System.out.println("<" + args.get(i) + ">");
+        }
+    }
+}
