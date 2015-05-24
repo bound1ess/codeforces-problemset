9cac9407991f196a0a82148ee6603cd0ad9f730c
bound1ess <bound1ess.dev@gmail.com>
2015-05-24 20:48:33 +0600

    Add 245b.

---
+++ 245/b.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class b {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String addr = in.nextLine();
+        in.close();
+
+        StringBuilder newAddr = new StringBuilder();
+
+        if (addr.substring(0, 3).equals("ftp")) {
+            newAddr.append("ftp://");
+            addr = addr.substring(3);
+        } else {
+            newAddr.append("http://");
+            addr = addr.substring(4);
+        }
+
+        int pivot = addr.indexOf("ru", 1);
+
+        newAddr.append(addr.substring(0, pivot));
+        //newAddr.append(".ru/");
+        newAddr.append(".ru");
+
+        if (addr.length() > pivot + 2) {
+            newAddr.append("/");
+            newAddr.append(addr.substring(pivot + 2));
+        }
+
+        System.out.println(newAddr.toString());
+    }
+}
