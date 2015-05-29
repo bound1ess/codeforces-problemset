7165c6eb5358ba7161a992016f9892b8c3221f67
bound1ess <bound1ess.dev@gmail.com>
2015-05-29 15:20:10 +0600

    Add 447a.

---
+++ 447/a.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class a {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, Integer> hash = new HashMap<>();
+
+        int p = sc.nextInt(), n = sc.nextInt();
+        int[] x = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            int key = x[i] % p;
+
+            if ( ! hash.containsKey(key)) {
+                hash.put(key, 1);
+            } else {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
