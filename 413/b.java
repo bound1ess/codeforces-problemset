9ba3649485c9dc555d7a13b446c193650fc7c5ed
bound1ess <bound1ess.dev@gmail.com>
2015-06-04 19:34:57 +0600

    Add 413b.

---
+++ 413/b.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class b {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // staff, chats, log events
+        int n = sc.nextInt(), m = sc.nextInt(), k = sc.nextInt();
+
+        int[][] membership = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // member => chat
+                membership[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] members = new int[n], chats = new int[m];
+
+        for (int i = 0; i < k; i++) {
+            int x = sc.nextInt() - 1, y = sc.nextInt() - 1;
+
+            chats[y]++; members[x]--;
+        }
+
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (membership[i][j] > 0) {
+                    members[i] += chats[j];
+                }
+            }
+
+            System.out.print(members[i]);
+            System.out.print(i + 1 < n ? " " : "\n");
+        }
+    }
+}
