db60b1a6703c8bbf9a3eedf05735918eef5bc367
bound1ess <bound1ess.dev@gmail.com>
2015-06-04 13:18:22 +0600

    Add 125b.

---
+++ 125/b.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class b {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String xml = sc.nextLine();
+        sc.close();
+
+        int level = -1;
+
+        for (String token: xml.split("<|>")) {
+            if (token.isEmpty()) {
+                continue;
+            }
+
+            if ( ! token.startsWith("/")) {
+                level++;
+            }
+
+            for (int i = 0; i < level; i++) {
+                System.out.print("  ");
+            }
+
+            System.out.println("<" + token + ">");
+
+            if (token.startsWith("/")) {
+                level--;
+            }
+        }
+    }
+}
