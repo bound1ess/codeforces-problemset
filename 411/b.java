99b172178a123e64dbfda278f5f4c7f3d150c48c
bound1ess <bound1ess.dev@gmail.com>
2015-06-04 15:15:59 +0600

    Add 411b.

---
+++ 411/b.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class b {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), k = sc.nextInt();
+        int[][] calls = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                calls[i][j] = sc.nextInt();
+            }
+        }
+
+        sc.close();
+
+        int[] deadlock = new int[n], answer = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            int[] counter = new int[k + 1]; // 1-based indices
+
+            for (int j = 0; j < n; j++) {
+                if (deadlock[j] > 0) {
+                    calls[j][i] = deadlock[j];
+                }
+
+                counter[calls[j][i]]++;
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (calls[j][i] > 0 && counter[calls[j][i]] > 1 && deadlock[j] < 1) {
+                    answer[j] = i + 1;
+                    deadlock[j] = calls[j][i];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+}
