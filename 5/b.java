9bf72d5dbf7635c3715d5767d3e0823cceda193b
bound1ess <bound1ess.dev@gmail.com>
2015-06-06 02:53:33 +0600

    Add 5b.

---
+++ 5/b.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class b {
+
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> msg = new ArrayList<>();
+        int maxLength = 0;
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+
+            //if (line.equals("stop")) break;
+
+            msg.add(line);
+            maxLength = Math.max(maxLength, line.length());
+        }
+
+        int blockLength = maxLength + 2;
+        int placeLeft = 1;
+
+        System.out.println(repeat('*', blockLength));
+
+        for (String line: msg) {
+            int whitespaces = maxLength - line.length();
+
+            if (whitespaces % 2 == 0) {
+                System.out.print('*' + repeat(' ', whitespaces / 2));
+                System.out.print(line);
+                System.out.println(repeat(' ', whitespaces / 2) + '*');
+            } else {
+                int alignLeft, alignRight;
+                //System.out.println("Block length: " + blockLength);
+                //System.out.println("Line length: " + line.length());
+
+                if (placeLeft > 0) {
+                    alignLeft = whitespaces / 2;
+                    alignRight = whitespaces - alignLeft;
+                } else {
+                    alignRight = whitespaces / 2;
+                    alignLeft = whitespaces - alignRight;
+                }
+
+                //System.out.println("left: " +alignLeft+ ", right: " +alignRight);
+
+                System.out.print('*' + repeat(' ', alignLeft));
+                System.out.print(line);
+                System.out.println(repeat(' ', alignRight) + '*');
+
+                // inverse
+                placeLeft = 1 - placeLeft;
+            }
+        }
+
+        System.out.println(repeat('*', blockLength));
+    }
+
+    private static String repeat(char chr, int n) {
+        char[] chars = new char[n];
+
+        // Arrays.fill
+        for (int i = 0; i < n; i++) {
+            chars[i] = chr;
+        }
+
+        return new String(chars);
+    }
+}
