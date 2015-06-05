873aef028d8963ce554fdd93c2b2e62c0b33a723
bound1ess <bound1ess.dev@gmail.com>
2015-06-05 17:49:24 +0600

    Add 312a.

---
+++ 312/a.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class a {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] a = new String[n];
+
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextLine();
+        }
+
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            boolean end = a[i].endsWith("lala."), begin = a[i].startsWith("miao.");
+
+            if (begin && end) {
+                System.out.println("OMG>.< I don't know!");
+                continue;
+            }
+
+            if (end) {
+                System.out.println("Freda's");
+            }
+
+            if (begin) {
+                System.out.println("Rainbow's");
+            }
+
+            if ( ! begin && ! end) {
+                System.out.println("OMG>.< I don't know!");
+            }
+        }
+    }
+}
