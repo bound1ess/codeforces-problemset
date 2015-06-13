e0f5c490d2412af4dd02942d79f3c1bfb7329f89
bound1ess <bound1ess.dev@gmail.com>
2015-06-13 22:31:39 +0600

    Add 381a.

---
+++ 381/a.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+
+int a[1000];
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int l = 0, r = n - 1, a_sum = 0, b_sum = 0, id = 0;
+
+    while (l <= r) {
+        int max;
+
+        if (a[l] < a[r]) {
+            max = a[r], r--;
+        } else {
+            max = a[l], l++;
+        }
+
+        if (id < 1) {
+            a_sum += max;
+        } else {
+            b_sum += max;
+        }
+
+        id = 1 - id;
+    }
+
+    printf("%d %d\n", a_sum, b_sum);
+
+    return 0;
+}
