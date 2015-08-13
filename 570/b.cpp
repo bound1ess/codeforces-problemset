ac7f3260104dbab1ad53ecb6aeb7913f6f46c7e4
bound1ess <bound1ess.dev@gmail.com>
2015-08-14 01:43:15 +0600

    Add 570b.

---
+++ 570/b.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main() {
+  int n, m;
+  scanf("%d%d", &n, &m);
+  if (n < 2) {
+    printf("1\n");
+  } else if (m - 1 < n - m) {
+    printf("%d\n", m + 1);
+  } else {
+    printf("%d\n", m - 1);
+  }
+  return 0;
+}
