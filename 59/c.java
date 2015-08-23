6205dbb4bc77b9cd1272c04c9e5e038003d2ba04
bound1ess <bound1ess.dev@gmail.com>
2015-08-23 16:33:44 +0600

    Whoops, rewrite 59c.

--- 59/c.java
+++ 59/c.java
@@ -2,67 +2,63 @@ import java.util.Scanner;
 
 public class c {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+  private static int MAGIC = 128;
 
-        int k = Integer.parseInt(sc.nextLine());
-        char[] chars = sc.nextLine().toCharArray();
-        boolean[] used = new boolean[128];
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int k = Integer.parseInt(sc.nextLine());
+    char[] chars = sc.nextLine().toCharArray();
+    boolean[] used = new boolean[MAGIC];
+    sc.close();
 
-        sc.close();
+    for (int i = 0; i < chars.length; ++i) {
+      if (chars[i] == '?') {
+        continue;
+      }
 
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == '?') {
-                continue;
-            }
+      int j = chars.length - i - 1;
 
-            int j = chars.length - i - 1;
-
-            if (chars[j] == '?') {
-                chars[j] = chars[i];
-            } else {
-                if (chars[i] != chars[j]) {
-                    System.out.println("IMPOSSIBLE");
-                    return;
-                }
-            }
+      if (chars[j] == '?') {
+        chars[j] = chars[i];
+      } else {
+        if (chars[i] != chars[j]) {
+          System.out.println("IMPOSSIBLE");
+          return;
         }
-
-        for (int i = 0; i < chars.length; i++) {
-            used[(int) chars[i]] = true;
-        }
-
-        int chr = k - 1 + 'a';
-
-        for (int i = chars.length / 2; i > -1; i--) {
-            while (used[chr] && chr >= 'a') {
-                chr--;
-            }
-
-            if (chars[i] != '?') {
-                continue;
-            }
-
-            int j = chars.length - i - 1;
-
-            if (chr >= 'a') {
-                chars[i] = (char) chr;
-                chars[j] = (char) chr;
-                chr--;
-            } else {
-                chars[i] = 'a';
-                chars[j] = 'a';
-            }
-        }
-
-        while (used[chr] && chr >= 'a') {
-            chr--;
-        }
-
-        if (chr >= 'a') {
-            System.out.println("IMPOSSIBLE");
-        } else {
-            System.out.println(new String(chars));
-        }
+      }
     }
+
+    for (int i = 0; i < chars.length; ++i) {
+      used[(int) chars[i]] = true;
+    }
+
+    int chr = k - 1 + 'a';
+
+    for (int i = chars.length / 2; i >= 0; --i) {
+      while (used[chr] && chr >= 'a') {
+        --chr;
+      }
+
+      if (chars[i] != '?') {
+        continue;
+      }
+
+      int j = chars.length - i - 1;
+
+      if (chr >= 'a') {
+        chars[i] = (char) chr;
+        chars[j] = (char) chr;
+        --chr;
+      } else {
+        chars[i] = 'a';
+        chars[j] = 'a';
+      }
+    }
+
+    while (used[chr] && chr >= 'a') {
+      --chr;
+    }
+
+    System.out.println(chr >= 'a' ? "IMPOSSIBLE" : new String(chars));
+  }
 }
