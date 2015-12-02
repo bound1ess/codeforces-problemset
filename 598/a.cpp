042c98ee64957eef2434275f1b3c1edc7a5f5fa3
bound1ess <bound1ess.dev@gmail.com>
2015-12-02 22:24:54 +0600

    Update 598a.

--- 598/a.cpp
+++ 598/a.cpp
@@ -2,31 +2,21 @@
 
 using namespace std;
 
-const int N = 30, M = 123;
-long long pow[N];
-int a[M];
-
-inline long long solve(int n) {
-  long long sum = n * (n + 1ll) / 2ll, remove = 0ll;
-  for (int i = 0; i < N; ++i) {
-    if (pow[i] <= n) {
-      remove += pow[i] * 2ll;
-    }
-  }
-  return sum - remove;
-}
+const int N = 123;
+long long a[N];
 
 int main() {
   int t;
   cin >> t;
-  for (int i = 0; i < N; ++i) {
-    pow[i] = 1ll << i;
-  }
   for (int i = 0; i < t; ++i) {
     cin >> a[i];
   }
   for (int i = 0; i < t; ++i) {
-    cout << solve(a[i]) << endl;
+    long long ans = a[i] * (a[i] + 1ll) / 2ll;
+    for (long long j = 1ll; j <= a[i]; j *= 2ll) {
+      ans -= j * 2ll;
+    }
+    cout << ans << endl;
   }
   return 0;
 }
