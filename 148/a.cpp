1c30575d3f45041a67791688c1802300085dbbd8
bound1ess <bound1ess.dev@gmail.com>
2016-01-09 19:24:01 +0600

    Add 148a.

---
+++ 148/a.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+  int k, l, m, n, d, ans = 0;
+  scanf("%d%d%d%d%d", &k, &l, &m, &n, &d);
+  for (int i = 1; i <= d; ++i) {
+    if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) {
+      ++ans;
+    }
+  }
+  printf("%d\n", ans);
+  return 0;
+}
